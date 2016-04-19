8045a2528bad42f286900425d95d36c3b1295199
ejb <mail@elliotbentley.com>
2016-04-19 11:25:56 +0100

    Initial public commit

---
+++ Readme.md
@@ -0,0 +1,115 @@
+# scrollWatcher 📜👀
+
+A small JavaScript library for scroll-based data-driven graphics (without any nasty [scrolljacking](http://blog.arronhunt.com/post/66973746030/stop-scrolljacking)). scrollWatcher sticks an element at a fixed position onscreen, and then provides the distance scrolled through its (much taller) parent as a percentage.
+
+- [Demo here](http://wsj.github.io/scroll-watcher/)
+
+As seen on WSJ.com in [How Fed Rates Move Markets](http://graphics.wsj.com/reacting-to-fed-rates/) and [What ECB Stimulus Has Done](http://graphics.wsj.com/what-ecb-qe-stimulus-has-done/).
+
+## Is scrollWatcher the right library for you?
+
+- Want something to always stick on the page? Use [CSS `position: fixed;`](https://css-tricks.com/almanac/properties/p/position/#article-header-id-2).
+- Want something to stick when you scroll past it? Use [jQuery fixto plugin](https://github.com/bbarakaci/fixto) or [jQuery Sticky](https://github.com/garand/sticky).
+- Want a sticky header that hides on scroll? Use [headroom.js](http://wicky.nillia.ms/headroom.js/).
+- Want to trigger events on scroll? Use [Waypoints](http://imakewebthings.com/waypoints/).
+- **If you want to use scroll as a way of interacting with a data-driven graphic without scrolljacking, use scrollWatcher.**
+
+## Quickstart
+
+1. Include [jQuery](http://jquery.com) and the sticky-positioning [fixto plugin](https://github.com/bbarakaci/fixto) on the page.
+
+2. In your HTML, you'll need a tall outer element, with one much shorter element inside of it.
+
+    ```html
+    <div class="outer" style="height: 2000px;"></div>
+        <div class="inner"></div>
+    </div>
+    ```
+
+3. In your JavaScript, you'll need to call `scrollWatcher` with a configuration object using `parent` and `onUpdate` arguments. The function passed into `onUpdate` will run every 20 miliseconds.
+
+    ```js
+    scrollWatcher({
+        parent: '.outer',
+        onUpdate: function( scrollPercent, parentElement ){
+            $('.inner').text('Scrolled '+scrollPercent+'% through the parent.');
+        }
+    });
+    ```
+
+## Examples
+
+Check out the source code to see how these are used.
+
+- [Basic demo](http://wsj.github.io/scroll-watcher/)
+- [Using a D3 scale](http://wsj.github.io/scroll-watcher/examples/d3-scale.html)
+- [Using a D3 time scale](http://wsj.github.io/scroll-watcher/examples/d3-scale-time.html)
+- [Checking if the user has got there yet](http://wsj.github.io/scroll-watcher/examples/active.html)
+
+## Other features
+
+### Starting and stopping
+
+If you create a new instance of scrollWatcher:
+
+```js
+var myWatcher = scrollWatcher({
+    onUpdate: function( scrollPercent, parentElement ){
+        console.log('Scrolled '+scrollPercent+'% through the parent.');
+    },
+    parent: '.outer'
+});
+```
+
+... you can then start, pause and stop at any time.
+
+```js
+// stop checking but keep stuck
+myWatcher.pause();
+
+// stop checking and unstick
+myWatcher.stop();
+
+// start checking and restick
+myWatcher.start();
+```
+
+### Check if active
+
+There are two read-only properties:
+
+- `active` is true when the scrollWatcher instance is currently onscreen (and running). 
+
+    ```js
+    var isActive = myWatcher.active;
+    ```
+        
+- `hasBeenActive` is true when the scrollWatcher instance has been onscreen (and run) at least once.
+
+    ```js
+    var isOrWasActive = myWatcher.hasBeenActive;
+    ```
+
+You may want to use these to (for example) hide a "keep scrolling!" message.
+
+### Check for device support
+
+Use `scrollWatcher.supported()` to check whether or not scrollWatcher will work in the current browser.
+
+## Browser support
+
+scrollWatcher works on all modern browsers, including IE 9 and up. However, **it does not work on iOS 7 and lower** due to the way Safari handles CSS `position: fixed;`.
+
+## Testing
+
+To run tests, open [`tests.html`](http://github.com/wsj/scroll-watcher/tests.html) in your browser and wait a couple of seconds.
+
+## Version history
+
+**v1.0.0** (April 19, 2016)
+
+- Initial public release
+
+## License
+
+[ISC](/LICENSE)
---
+++ scroll-watcher.js
@@ -0,0 +1,154 @@
+/*
+scrollWatcher v1.0.0
+Developed by Elliot Bentley for The Wall Street Journal
+Released under the ISC license
+*/
+function scrollWatcher(opts){
+    var interval;
+    var $outer = $(opts.parent);
+    var $inner = $(opts.parent).children().eq(0);
+    var that = {
+        active: false,
+        hasBeenActive: false
+    };
+
+    // use jQuery fixTo plugin to make element sticky
+    var outerId = Math.random().toString().replace('.','');
+    
+    if (typeof opts.onUpdate !== 'function') {
+        throw('scrollWatcher needs an "onUpdate" callback.');
+    }
+    
+    // this runs on each interval
+    var maxedOut = false;
+    var previousPos = 0;
+    var onTick = function(){
+        checkInnerHeight();
+        var scrollDistance = getScrollDistance();
+        var scrollPos = getScrollPos(scrollDistance);
+        var cappedScrollPos = capPercentage(scrollPos);
+        var scrollPosMaxOrMore = ( (scrollPos >= 100) || (scrollPos <= 0) );
+        
+        // 'maxedOut' is true when the scrollPos is outside of 0-100
+        // and has run once at that maxed-out scroll position.
+        // It improves performance by only running the callback when necessary
+        if (scrollPos !== previousPos) {
+            maxedOut = false;
+        }
+        if (!scrollPosMaxOrMore) {
+            // 'active' property is for external API
+            that.active = true;
+            that.hasBeenActive = true;            
+            maxedOut = false;
+        }
+        if (!maxedOut) {
+            // run callback function as specified by user
+            opts.onUpdate( cappedScrollPos, $outer );
+        }
+        if (scrollPosMaxOrMore) {
+            maxedOut = true;
+            // 'active' property is for external API
+            that.active = false;
+        }
+        previousPos = scrollPos;
+    }    
+    // gets scroll position as pixels from top of parent
+    var getScrollDistance = function(){
+        // cross-browser compatibility functionality from MDN
+        // https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
+        var supportPageOffset = window.pageXOffset !== undefined;
+        var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+        var y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+        return y - $outer.offset().top;
+    }
+    // gets scroll position as % of container
+    var getScrollPos = function(scrollDistance){
+        var scrollPerc = ( scrollDistance / ( $outer.height() - $(window).height() ) ) * 100;
+        return scrollPerc;
+    }
+    // toggles stickiness of 'inner' element
+    var stickUnstick = function(scrollPos){
+        if (scrollPos === 0){
+            $inner.css('position','relative');
+        } else if (scrollPos === 100) {
+            $inner.css('position','absolute');
+            $inner.css('bottom','0');
+        } else {
+            $inner.css('position','fixed');
+        }
+    }
+    
+    // prevent number from going under 0% or over 100%
+    var capPercentage = function(scrollPerc){
+        if (scrollPerc > 100) {
+            scrollPerc = 100;
+        } else if (scrollPerc < 0) {
+            scrollPerc = 0;
+        }
+        return scrollPerc;
+    }
+    
+    // check if child element is taller than window height
+    var previousInnerHeight;
+    var checkInnerHeight = function(){
+        var newInnerHeight = $inner.height();
+        if (newInnerHeight === previousInnerHeight) {
+            return;
+        }
+        $inner.css( 'height', 'auto' );
+        newInnerHeight = $inner.height();
+        var overflow = $(window).height() - newInnerHeight;
+        if (overflow >= 0) {
+            newInnerHeight = $(window).height() - (overflow+5);
+            $inner.css( 'height', newInnerHeight );
+            $inner.css( 'margin-bottom', overflow );
+        }
+        previousInnerHeight = newInnerHeight;
+    }
+        
+    // stop checking and unstick
+    that.stop = function(){
+        clearInterval( interval );
+        $outer.attr('id','');
+        $inner.fixTo('destroy');
+        return this;
+    };
+    // stop checking but keep stuck
+    that.pause = function(){
+        clearInterval( interval );
+        return this;
+    };
+    // starts watching for scroll movement
+    that.start = function(){
+        // sticky stuff
+        $outer.attr('id',outerId);
+        $inner.fixTo('#'+outerId);
+        interval = setInterval(onTick,20);
+        return this;
+    }
+    // could be a useful alias
+    that.resume = that.start;
+
+    // start watching immediately
+    that.start();
+        
+    return that;
+}
+
+// does current device support scrollWatcher? returns true or false
+scrollWatcher.supported = function(){
+    
+    // feature sniff for old browsers
+    return ('indexOf' in []);
+            
+    // before iOS 8, CSS position fixed did not work
+    var iOS_matches = navigator.userAgent.match(/(iPad|iPhone|iPod touch);.*CPU.*OS (\d_\d)/i);
+    if (iOS_matches) {
+        var version = parseFloat(iOS_matches[iOS_matches.length-1].replace('_','.'));
+        if (version < 8) {
+            return false;
+        }
+    }
+    
+    return true;
+}
